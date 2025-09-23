f48a3650e2099390a4cc1fea352dfa6a3550ad4e
karen-castillo-laplateforme <karen.castillo@laplateforme.io>
2025-09-23 11:49:49 +0200

    job01 done

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+  $str = "LaPlateforme";
+  echo $str . "<br>";
+  $str2 = "Vive";
+  $str3 = "!";
+  echo $str2 . " " . $str . " " . $str3;
+
+  $val = 6;
+  echo "<br>" . $val;
+  echo "<br>" . ($val + 4);
+
+  $myBool = true;
+  echo "<br>" . $myBool;
+  $myBool = false;
+  echo "<br>" . $myBool;
+  var_dump($myBool);
+  ?>
