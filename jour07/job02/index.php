68ad6013d4e522aa662e1bf052a2b3b7d31848b9
karen-castillo-laplateforme <karen.castillo@laplateforme.io>
2025-10-01 11:34:26 +0200

    job03 done

---
+++ jour07/job02/index.php
@@ -0,0 +1,11 @@
+<?php
+  function bonjour($jour){
+    if($jour === true){
+        echo "Bonjour";
+    } elseif($jour === false){
+        echo "Bonsoir";
+    }
+  }
+
+  bonjour(true);
+?>
---
+++ jour07/job03/index.php
@@ -0,0 +1,8 @@
+<?php
+function getHello(){
+    return "Hello LaPlateforme!";
+}
+
+$return = getHello();
+echo $return;
+?>
