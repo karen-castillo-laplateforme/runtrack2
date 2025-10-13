6f98d8e0fb9a062fd65e5f5be29ae84c89b7babe
karen-castillo-laplateforme <karen.castillo@laplateforme.io>
2025-10-13 13:50:51 +0200

    job03 done

---
+++ jour10/job03/index.php
@@ -0,0 +1,58 @@
+<?php
+$connexion = mysqli_connect('localhost', 'root');
+$db = mysqli_select_db($connexion, 'jour09');
+
+if(!$connexion){
+  die("Connexion échouée à la BDD : " . mysqli_connect_error());
+}
+
+$cmd = "SELECT prenom, nom, naissance FROM etudiants WHERE sexe = 'Femme'";
+
+$db_request = mysqli_query($connexion, $cmd);
+
+if(!$db_request){
+  die("Erreur requête : " . mysqli_error($connexion));
+}
+
+/* Récup nom des champs */
+$all_fields = mysqli_fetch_fields($db_request);
+
+$field_names = [];
+foreach($all_fields as $f){
+  $field_names[] = $f->name;
+}
+
+/* Récup data des colonnes */
+$students = mysqli_fetch_all($db_request, MYSQLI_ASSOC);
+var_dump($students);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <table>
+    <thead>
+      <tr>
+        <?php foreach($field_names as $n): ?>
+          <td><?= ucfirst($n); ?></td>
+        <?php endforeach; ?>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($students as $s):?>
+        <?php var_dump($s); ?>
+        <tr>
+          <td><?= $s["prenom"] ?></td>
+          <td><?= $s["nom"]?></td>
+          <td><?= $s["naissance"]?></td>
+        </tr>
+      <?php endforeach; ?>   
+    </tbody>
+  </table>
+</body>
+</html>
