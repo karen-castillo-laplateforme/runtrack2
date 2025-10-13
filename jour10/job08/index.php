40da6f6fc1743e0faf9a82f13e033c05afaff19c
karen-castillo-laplateforme <karen.castillo@laplateforme.io>
2025-10-13 16:48:47 +0200

    job08 done

---
+++ jour10/job08/index.php
@@ -0,0 +1,49 @@
+<?php
+$connexion = mysqli_connect('localhost', 'root');
+$db = mysqli_select_db($connexion, 'jour09');
+
+if(!$connexion){
+  die("Connexion échouée : " . $connexion);
+}
+
+$request = "SELECT SUM(capacite)
+FROM salles";
+
+$db_request = mysqli_query($connexion, $request);
+
+if(!$db_request){
+  die("Requête échouée : " . mysqli_error($connexion));
+}
+
+/* Récup data des colonnes */
+$row = mysqli_fetch_row($db_request);
+var_dump($row);
+$capacite = $row[0];
+
+// Libération du résultat et fermeture de la connexion
+mysqli_free_result($db_request);
+mysqli_close($connexion);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <table>
+    <thead>
+      <tr>
+        <td>Capacité</td>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+          <td><?= $capacite ?></td>
+        </tr>  
+    </tbody>
+  </table>
+</body>
+</html>
